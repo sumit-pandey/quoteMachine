396a171bfeb1289d20ba697d5bd06d2fc15cce87
Sumit Pandey <sumit_pandey@outlook.com>
2019-06-02 04:08:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var quote;
+    var author;
+    function getQuote(){
+        fetch('https://quota.glitch.me/random')
+            .then(response => response.json())
+            .then(data => {
+                quote = data.quoteText;
+                author = data.quoteAuthor;   
+                $(".quote-box-inner").text('"' + quote +  '"');
+                $(".inner-footer").text("-" + author)   
+    })
+  
+}
+getQuote();
+$("#get-quote").on('click', function(){
+    getQuote()
+    $('body')
+})
+
+})
+
+
